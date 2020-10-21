c9e0dd0319a878ba3d3836e00d7909baeb0c3f3f
sharmaritik038 <73228474+sharmaritik038@users.noreply.github.com>
2020-10-21 19:37:01 +0530

    Add files via upload

---
+++ product.java
@@ -0,0 +1,35 @@
+package com.sportsArmyFoundation.client;
+
+public class product {
+	
+		private int productid;
+		private String productname;
+		private String desc;
+		private double price;
+		public int getproductid() {
+			return productid;
+		}
+		public void setproductid(int productid) {
+			this.productid=productid;
+		}
+		public String getproductname() {
+			return productname;
+		}
+		public void setproductname(String productname) {
+			this.productname=productname;
+		}
+		public String getdis() {
+			return desc;
+		}
+		public void setdesc(String desc) {
+			this.desc=desc;
+		}
+		public double getprice() {
+			return price;
+		}
+		public void setprice(double price) {
+			this.price=price;
+			
+		}
+
+}
